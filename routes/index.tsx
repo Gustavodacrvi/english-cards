12ed7a2c632199cf8b13fd722a10bf427c2d9566
Gustavodacrvi <gustavodacrvi@gmail.com>
2020-05-05 13:06:19 -0300

    stack navigator setup

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+
+
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import Authentication from './../views/Authentication'
+
+
+export default createAppContainer(
+  createStackNavigator({
+    Authentication: {
+      screen: Authentication,
+    },
+  }, {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
+  })
+)
---
+++ views/Authentication.tsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import { View, Text } from "react-native"
+
+import { backgroundColor } from './../styles/colors'
+
+function Authentication() {
+  return (
+    <View style={{backgroundColor, height: '100%'}}>
+      <Text>
+        Authentication
+      </Text>
+    </View>
+  )
+}
+
+export default Authentication
