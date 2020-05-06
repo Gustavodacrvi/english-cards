cee84a3bd78b25bd883b352abe12a8fb67ba3623
vinibispo <vini.bispo015@gmail.com>
2020-05-06 00:11:41 -0300

    Adding basic context menu

--- src/background.js
+++ src/background.js
@@ -1,3 +1,4 @@
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log('Hello from the background')
 })
+browser.contextMenus.create({'id': 'menuFlash',  'title': 'Adicionar ao FlashCard', 'contexts': ['selection']})
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -4,14 +4,6 @@
   </div>
 </template>
 
-<script>
-// import HomeVue from './views/Home.vue'
-export default {
-  // components: {
-    // HomeVue
-  // },
-}
-</script>
 
 <style>
 :root {
