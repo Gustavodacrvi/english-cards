077a9fc83425c6ac8c21b19577642305c4abcc64
vinibispo <vini.bispo015@gmail.com>
2020-05-18 14:20:40 -0300

    Identation problem fixed

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,28 +1,29 @@
 <template>
-    <base-form-vue >
-      <transition
-        name="input-t"
-      >
-        <input-vue v-if='firstTime' type="text" v-model="user.username" placeholder="Nome de usuário:"/>
-      </transition>
-      <input-vue type="email" v-model="user.email" placeholder="E-mail:"/>
-      <input-vue type="password" v-model="user.password" placeholder="Senha:"/>
-      <button-vue text="Entrar" v-if="!firstTime" @click.native="login" :color="{'back': '#FFF', 'text': '#525A79'}" :border="true" />
-      <button-vue text="Criar conta" @click.native="signUp" v-if="firstTime" :color="{'back': '#FFF', 'text': '#525A79'}" :border="true" />
-      <span v-if="!firstTime" @click="openForgotPasswordPopup">Esqueceu a senha?</span>
-       <div v-if="firstTime" class="termos">	
-          <div class="wrapper">	
-            <span>	
-                Ao criar a conta você concorda com os <span style="display: inline-block"></span><span class="yellow" @click="goToThermsPage">termos de uso</span> e a <span class="yellow" @click="goToPrivacyPolicyPage">política de privacidade</span>	
-            </span>	
-          </div>	
+  <base-form-vue >
+    <transition
+       name="input-t" >
+       <input-vue v-if='firstTime' type="text" v-model="user.username" placeholder="Nome de usuário:"/>
+    </transition>
+    <input-vue type="email" v-model="user.email" placeholder="E-mail:"/>
+    <input-vue type="password" v-model="user.password" placeholder="Senha:"/>
+    <button-vue text="Entrar" v-if="!firstTime" @click.native="login" :color="{'back': '#FFF', 'text': '#525A79'}" :border="true" />
+    <button-vue text="Criar conta" @click.native="signUp" v-if="firstTime" :color="{'back': '#FFF', 'text': '#525A79'}" :border="true" />
+    <span v-if="!firstTime" @click="openForgotPasswordPopup">Esqueceu a senha?</span>
+    <div v-if="firstTime" class="termos">  
+      <div class="wrapper">
+        <span>
+          Ao criar a conta você concorda com os
+          <span style="display: inline-block"></span>
+          <span class="yellow" @click="goToThermsPage">termos de uso</span>
+          e a <span class="yellow" @click="goToPrivacyPolicyPage">política de privacidade</span>
+        </span>
       </div>
-      <transition name="popup" >
-        <message-popup-vue v-if="!firstTime && popup" :color="color" :message="message" />          
-      </transition>
-    </base-form-vue>
+    </div>
+    <transition name="popup" >
+      <message-popup-vue v-if="!firstTime && popup" :color="color" :message="message" />
+    </transition>
+  </base-form-vue>
 </template>
-
 <script>
 import {mapState} from 'vuex'
 import BaseFormVue from './BaseForm.vue'
@@ -30,112 +31,111 @@ import InputVue from './Input.vue'
 import ButtonVue from '../Button.vue'
 import MessagePopupVue from '../MessagePopup.vue'
 export default {
-    props: ['firstTime'],
-    data(){
-        return {
-            counter: 0,
-            popup: false,
-            color: '#F00',
-            message: 'Você não pode enviar uma confirmação de email, tente novamente daqui a um segundo',
-            user: {
-              username: '',
-              email: '',
-              password: ''
-            }
-        }
-    },
-    components: {
-        BaseFormVue,
-        InputVue,
-        ButtonVue,
-        MessagePopupVue
-    },
-    watch: {
-      error() {
-        if(this.error) {
-          this.popup = true
-          this.color = '#F00',
-          this.message = this.error
-          setTimeout(()=> {
-            this.popup = false
-            this.message = ''
-          }, 5000)
-        }
+  props: ['firstTime'],
+  data(){
+    return {
+      counter: 0,
+      popup: false,
+      color: '#F00',
+      message: 'Você não pode enviar uma confirmação de email, tente novamente daqui a um segundo',
+      user: {
+        username: '',
+        email: '',
+        password: ''
       }
-    },
-    methods: {
-        openForgotPasswordPopup() {
-          if(this.user.email) {
-          this.$store.dispatch('sendResetPassword', this.user.email).then(()=> {
-              this.popup = true
-              if (this.counter < 1) {
-                this.color = '#88E976'
-                this.message = 'Um e-mail de confirmação foi mandado para e-mail dado.'
-                this.counter++
-              } else {
-                this.color = '#F00'
-                this.message = 'Você não pode enviar uma confirmação de email, tente novamente daqui a um segundo'
+    }
+  },
+  components: {
+    BaseFormVue,
+    InputVue,
+    ButtonVue,
+    MessagePopupVue
+  },
+  watch: {
+    error() {
+      if(this.error) {
+        this.popup = true
+        this.color = '#F00',
+        this.message = this.error
+        setTimeout(()=> {
+          this.popup = false
+          this.message = ''
+        }, 5000)
+      }
+    }
+  },
+  methods: {
+    openForgotPasswordPopup() {
+      if(this.user.email) {
+        this.$store.dispatch('sendResetPassword', this.user.email)
+          .then(()=> {
+            this.popup = true
+            if (this.counter < 1) {
+              this.color = '#88E976'
+              this.message = 'Um e-mail de confirmação foi mandado para e-mail dado.'
+              this.counter++
+            } else {
+              this.color = '#F00'
+              this.message = 'Você não pode enviar uma confirmação de email, tente novamente daqui a um segundo'
             }
-            setTimeout(()=>{
-            this.counter = 0
             setTimeout(()=>{
               this.counter = 0
-              this.popup = false
-            }, 5000)
-            }, 1000)
-          }
-          )
-          }
-          else {
-            setTimeout(()=> {
-              this.popup = true
-              this.message = "Por favor insira um email válido"
-              setTimeout(()=> {
+              setTimeout(()=>{
+                this.counter = 0
                 this.popup = false
               }, 5000)
             }, 1000)
           }
-          console.log(this.user.email)
-        },
-        goToThermsPage() {	
-            this.$router.push("/therms")	
-        },	
-        goToPrivacyPolicyPage() {	
-            this.$router.push('/privacy')	
-        },
-      async login() {
-         await this.$store.dispatch('login', this.user)
-					this.$router.push({'name': 'Dashboard'})
-        },
-     async signUp() {
-         await this.$store.dispatch('signUp', this.user)
-        }
+          )
+      }
+      else {
+        setTimeout(()=> {
+          this.popup = true
+          this.message = "Por favor insira um email válido"
+          setTimeout(()=> {
+            this.popup = false
+          }, 5000)
+        }, 1000)
+      }
+      console.log(this.user.email)
     },
-    computed: {
-      ...mapState(['error'])
+    goToThermsPage() {  
+      this.$router.push("/therms")  
+    },  
+    goToPrivacyPolicyPage() { 
+      this.$router.push('/privacy') 
+    },
+    async login() {
+      await this.$store.dispatch('login', this.user)
+      this.$router.push({'name': 'Dashboard'})
+    },
+    async signUp() {
+      await this.$store.dispatch('signUp', this.user)
     }
+  },
+  computed: {
+    ...mapState(['error'])
+  }
 }
 </script>
-
 <style scoped>
 span {
-    color: var(--white);
-    font-size: 13px;
-    font-weight: bold;
-    margin: 5px;
-    margin-left: 130px;
-    transition: color .15s, cursor .15s;
+  color: var(--white);
+  font-size: 13px;
+  font-weight: bold;
+  margin: 5px;
+  margin-left: 130px;
+  transition: color .15s, cursor .15s;
 }
 span:hover {
-    color: var(--yellow);
-    cursor: pointer;
+  color: var(--yellow);
+  cursor: pointer;
 }
-.termos {	
-    color: var(--white);	
-    font-size: 13px;	
-    font-weight: bold;	
+.termos { 
+  color: var(--white);
+  font-size: 13px;
+  font-weight: bold;
 }
-
 .input-t-enter, .input-t-leave-to {
   height: 0 !important;
   border: 0 solid transparent !important;
@@ -144,13 +144,12 @@ span:hover {
   transition-duration: .2s !important;
   margin-top: 0 !important;
 }
-
 .input-t-leave, .input-t-enter-to {
   height: 45px !important;
-  margin-top: 11px !important;
-  opacity: 1 !important;
   border: 3px solid var(--white) !important;
-  transition-duration: .2s;
+  opacity: 1 !important;
+  transition-duration: .2s !important;
+  margin-top: 11 !important;
 }
 .wrapper {
   display: flex;
@@ -171,7 +170,6 @@ span:hover {
 .yellow {
   color: var(--yellow);
 }
-
 .popup-enter {
   transform: translateY(100%);
   opacity: .5;
@@ -191,4 +189,5 @@ span:hover {
   transform: translateY(150%);
   transition-duration: .2s;
 }
+
 </style>
